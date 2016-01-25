c32c49f8125c594e47b3529ef75bb4a625832bbd
Yuriy <rm1945@yandex.ru>
2016-01-25 21:43:50 +0300

    Move root to containers

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,10 +1,9 @@
-
 import "phoenix_html"
 import React from "react"
 import { render } from "react-dom"
 
 // import socket from "./socket"
 
-import Root from "root"
+import Root from "containers/root"
 
 render(React.createElement(Root), document.getElementById("root"))
