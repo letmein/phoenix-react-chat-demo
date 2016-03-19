220977891a0b16d2098799c32b795c4488714641
Yuriy <rm1945@yandex.ru>
2016-03-19 21:05:08 +0300

    Test current user reducer

---
+++ web/static/js/reducers/current-user.test.js
@@ -0,0 +1,23 @@
+import { expect } from "chai"
+
+import subject from "./current-user"
+
+describe("current user reducer", () => {
+  it('returns default state', () => {
+    const action = {}
+    const result = subject(undefined, action)
+
+    expect(result).to.equal(null)
+  })
+
+  it('saves the user id', () => {
+    const state = null
+    const action = {
+      type: 'AUTHENTICATE_USER',
+      payload: { id: 'USER_ID' }
+    }
+    const result = subject(state, action)
+
+    expect(result).to.equal('USER_ID')
+  })
+})
