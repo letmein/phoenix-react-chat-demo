7d7892a0b852186852c43790083532701ef57636
Yuriy <rm1945@yandex.ru>
2016-03-28 23:31:33 +0300

    Display time

--- web/static/js/components/message-list/component.jsx
+++ web/static/js/components/message-list/component.jsx
@@ -1,7 +1,15 @@
 import React from "react"
 import _ from "lodash"
+import moment from "moment"
 
 import { UserAvatar } from ".." 
+
+
+function TimeStamp(props) {
+  const { value, className } = props
+  const formatted = moment(value).format("llll")
+  return (<span className={className}>{formatted}</span>)
+}
 
 export default (props) => {
   const { messages, className } = props
@@ -9,6 +17,8 @@ export default (props) => {
     <li className="message-list__message" key={message.id}>
       <UserAvatar imageUrl={message.user.avatar_url} className="message-list__avatar"/>
       <span className="message-list__username">{message.user.login}</span>
+      @
+      <TimeStamp className="message-list_time" value={message.sent_at}/>
       <br/>
       <span className="message-list__message-body">{message.text}</span>
     </li>
