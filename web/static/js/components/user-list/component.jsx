b1c08f17841453fae108882af0dcfb3e637b8dce
Yuriy <rm1945@yandex.ru>
2016-04-02 22:11:37 +0300

    Refactor UserList component

--- web/static/js/components/user-list/component.jsx
+++ web/static/js/components/user-list/component.jsx
@@ -3,15 +3,17 @@ import _ from "lodash"
 
 import { UserAvatar } from ".."
 
-export default (props) => {
-  const { users } = props
-  const userList = _.map(users, user => (
-    <li className="user-list__user" key={user.id}>
+const User = ({ user }) => {
+  return (
+    <li className="user-list__user">
       <UserAvatar imageUrl={user.avatar_url}/>
       <span className="user-list__name">{user.login}</span>
     </li>
-  ))
-  return (
-    <ul className="user-list">{userList}</ul>
   )
+}
+
+export default (props) => {
+  const { users } = props
+  const userList = _.map(users, user => <User user={user} key={user.id}/>)
+  return (<ul className="user-list">{userList}</ul>)
 }
